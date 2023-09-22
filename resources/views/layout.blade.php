22a0d3767f6ce0a183a37f4f5a5e2028940c40f2
Younes Guigaho <younesghu44@gmail.com>
2023-09-22 01:50:32 +0100

    edited color

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -18,7 +18,7 @@
                 theme: {
                     extend: {
                         colors: {
-                            laravel: "#ef3b2d",
+                            laravel: "#cfe2f3",
                         },
                     },
                 },
@@ -49,7 +49,7 @@
             @yield('content')
         </main>
         <footer class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center">
-        <p class="ml-2">Copyright &copy; 2022, All Rights reserved</p>
+        <p class="ml-2 text-black">Copyright &copy; 2022, All Rights reserved</p>
         <a
             href="create.html"
             class="absolute top-1/3 right-10 bg-black text-white py-2 px-5">
